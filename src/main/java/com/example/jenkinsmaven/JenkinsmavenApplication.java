c25e14577270d3e0e0126c6072c01f35dbc0a1aa
Priyam Gupta <priyam.gupta@yash.com>
2019-09-30 00:45:49 +0530

    third commit

--- src/main/java/com/example/jenkinsmaven/JenkinsmavenApplication.java
+++ src/main/java/com/example/jenkinsmaven/JenkinsmavenApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsmavenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinsmavenApplication.class, args);  
+		SpringApplication.run(JenkinsmavenApplication.class, args);   
 	}
 
 }
